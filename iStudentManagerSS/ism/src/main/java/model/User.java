f9a40961cdfc96023366f9a21323f7ece65eeb28
Aniket-Sarowar <97867160+Aniket-Sarowar@users.noreply.github.com>
2023-12-05 11:52:32 +0530

    Add files via upload

---
+++ iStudentManagerSS/ism/src/main/java/model/User.java
@@ -0,0 +1,19 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String username;
+    private String password;
+    private String role;
+
+    // Getters and setters
+}
+
